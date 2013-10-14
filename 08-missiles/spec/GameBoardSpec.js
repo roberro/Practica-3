8c94aa348e1b6fa816a3c4c857ee8b93a0e097ae
Roberto Rodríguez Alonso <rober8_8@hotmail.com>
2013-10-14 19:08:11 +0200

    Modificado missiles metodo draw

--- 08-missiles/spec/GameBoardSpec.js
+++ 08-missiles/spec/GameBoardSpec.js
@@ -40,3 +40,120 @@ Especificación: GameBoard debe
 
 
 */
+describe("Clase GameBoard", function(){
+
+    var canvas, ctx;
+
+    beforeEach(function(){
+	loadFixtures('index.html');
+
+	canvas = $('#game')[0];
+	expect(canvas).toExist();
+
+	ctx = canvas.getContext('2d');
+	expect(ctx).toBeDefined();
+
+    });
+
+    it("add", function(){
+
+	Game = {width: 320, height: 480};
+
+	var miNave = new GameBoard();
+	var obj = "nave";
+	
+	miNave.add(obj);
+
+	expect(miNave.add(obj)).toEqual("nave");
+
+    });
+
+    it("remove", function(){
+
+	Game = {width: 320, height: 480};
+
+	var miNave = new GameBoard();
+	var obj = "nave";
+	miNave.add(obj);
+	expect(miNave.objects[0]).toEqual("nave");	
+	miNave.resetRemoved(obj);
+	miNave.remove(obj);
+	miNave.finalizeRemoved(obj);
+	expect(miNave.objects[0]).toEqual(undefined);
+
+    });
+
+    it("iterate", function(){
+
+	Game = {width: 320, height: 480};
+
+	var miNave = new GameBoard();
+	miNave.objects = [{obj: function(){}, args: function(){}}];
+	spyOn(miNave.objects[0], "obj");
+	spyOn(miNave.objects[0], "args");
+
+	miNave.iterate("obj", "obj1", "obj2","obj3");
+	miNave.iterate("args", "args1", "args2","args3");
+	expect(miNave.objects[0].obj).toHaveBeenCalledWith("obj1", "obj2","obj3");
+	expect(miNave.objects[0].args).toHaveBeenCalledWith("args1", "args2","args3");
+    });
+
+
+    it("detect", function(){
+
+	var miNave = new GameBoard();
+	var obj = "nave";
+	
+	miNave.add(obj);
+
+	var func = {call: function(){}};
+	spyOn(func, "call");
+	miNave.detect(func)
+	waits(100);
+
+	expect(miNave.objects[0]).toBeTruthy();
+	expect(miNave.objects[1]).toBeFalsy();
+	
+    });
+
+    it("draw+step", function(){
+	var miNave = new GameBoard();
+
+	var board = {
+	    step: function (){},
+	    draw: function (){}
+	};
+
+	spyOn(board, "step");
+	spyOn(board, "draw");
+
+	miNave.add(board);
+	miNave.step(1);
+	miNave.draw(ctx);
+
+	waits(100); 
+	runs(function(){
+		expect(board.step).toHaveBeenCalled();
+		expect(board.draw).toHaveBeenCalled();
+	});
+    });
+
+    it("overlap", function(){
+	var miNave = new GameBoard();
+	var o1 = {x:2,y:2,h:1,w:1};
+	var o2 = {x:1,y:1,h:2,w:2};
+
+	miNave.add(o1);
+	miNave.add(o2);
+
+	expect(miNave.overlap(o1,o2)).toBeTruthy();	
+    });
+
+    it("collide", function(){
+	var miNave = new GameBoard();
+	miNave.objects = [{type:"1",x:2,y:2,h:1,w:1},{type:"2",x:1,y:1,h:2,w:2}];
+	var obj = {x:2,y:2,h:1,w:1};
+
+	expect(miNave.collide(obj,"1")).toBeTruthy();
+    });
+});
